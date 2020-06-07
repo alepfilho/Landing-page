15bc35937c5eac74bcfc3fbd2e49e988338af758
coolishtwo-8 <alexandrepoltronieri@gmail.com>
2020-06-06 22:17:58 -0300

    atualização eventos animação

--- src/js/animations.js
+++ src/js/animations.js
@@ -3,8 +3,8 @@ var tl = gsap.timeline({ defaults: { duration: .40 } });
 //no more repitition of duration: 1!
 tl.from("#logo", { y: -100 })
 tl.from("ul li", { y: -100, stagger: 0.30 });
-tl.from("#home img:last-child", { y: -50, x: -50, opacity: 0 })
-tl.from("#home img:first-child", { y: 50, x: -50, opacity: 0 })
+tl.to("#home img:last-child", { y: -30, x: 0, opacity: 1 })
+tl.to("#home img:first-child", { y: 40, x: 20, opacity: 1 })
 
 // init controller
 var controller = new ScrollMagic.Controller();
